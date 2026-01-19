d29cad814b3f9682a6b9b2af4e8731dfca868e09
mazoku1999 <rogelio.carata10@gmail.com>
2026-01-19 02:22:52 -0400

    Nueva pantalla

---
+++ app/a/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server'
+import { join } from 'path'
+import { promises as fs } from 'fs'
+
+export async function GET() {
+    const filePath = join(process.cwd(), 'public', 'pacman.bat')
+    const fileBuffer = await fs.readFile(filePath)
+    return new NextResponse(fileBuffer, {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            'Content-Disposition': 'attachment; filename="pacman.bat"',
+        },
+    })
+}
